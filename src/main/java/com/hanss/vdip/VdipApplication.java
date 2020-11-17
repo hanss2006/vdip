cdda701f22bc557a2fddd21a7482e9e7efd47e3b
hanss2006 <hanss@live.ru>
2020-11-17 16:38:56 +0300

    Создание пустого приложения Spring Boot

---
+++ src/main/java/com/hanss/vdip/VdipApplication.java
@@ -0,0 +1,13 @@
+package com.hanss.vdip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanss/vdip/VdipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanss.vdip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
